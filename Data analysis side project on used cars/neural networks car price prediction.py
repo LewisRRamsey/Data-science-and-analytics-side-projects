b16b5a854cad937f847849afa4344bcc6df1b19d
LewisRRamsey <lewis.ramsey18@gmail.com>
2025-09-14 15:12:06 +0100

    Adding neural networks file for price prediction

---
+++ Data analysis side project on used cars/neural networks car price prediction.py
@@ -0,0 +1,6 @@
+# Round odometer to nearest 1000 for accuracy balance
+# Use year, condition, cylinder, manufacturer and odometer from mapped vehicles
+# Split into test and train split
+# Train model
+# Test model
+# Evaluate model

ea229edff3e40d2fec05665cc9ad27b413767658
LewisRRamsey <lewis.ramsey18@gmail.com>
2025-10-28 23:48:56 +0000

    random forrest regression implementation

--- Data analysis side project on used cars/car_price_prediction_train.py
+++ Data analysis side project on used cars/car_price_prediction_train.py
@@ -1,20 +1,12 @@
-# Round odometer to nearest 1000 for accuracy balance
-# Use year, condition, cylinder, manufacturer and odometer from mapped vehicles
-# Split into test and train split
-# Train model
-# Test model
-# Evaluate model
-
 from pymongo import MongoClient
 import pandas as pd
 import numpy as np
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
 
 # Accessing dataset from MongoBD
 
@@ -39,20 +31,46 @@ def decomposing_used_car_data(mapped_vehicles_df: pd.DataFrame):
     decomp_vehicles_df = mapped_vehicles_df[['price', 'year', 'manufacturer', 'condition', 'cylinders', 'odometer']]
     return decomp_vehicles_df
 
-# splitting data randomly into test and training data using a 90/10 split
+# splitting data into test and training data using a 90/10 split
 def split_test_and_train_data(decomp_vehicles_df):
     None
     x = decomp_vehicles_df.drop(columns=['price']).values
     y = decomp_vehicles_df['price'].values
-    y = y.reshape(-1, 1)
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
     return x_train, x_test, y_train, y_test
 
 
-# preparing data
+def creating_decision_tree(info_train, price_train):
 
-mapped_vehicles_df = get_used_car_dataset()
-decomp_vehicles_df = decomposing_used_car_data(mapped_vehicles_df)
-x_train, x_test, y_train, y_test = split_test_and_train_data(decomp_vehicles_df)
+    # do further tweaks to model
+    dt_price_model = RandomForestRegressor(random_state=42, max_depth = 23)
+    dt_price_model.fit(info_train, price_train)
+    return dt_price_model
 
 
+def testing_decision_tree_model(dt_price_model, info_test, price_test):
+    price_pred = dt_price_model.predict(info_test)
+
+    mae = mean_absolute_error(price_test, price_pred)
+    mse = mean_squared_error(price_test, price_pred)
+    rmse = mse ** 0.5
+    r2 = r2_score(price_test, price_pred)
+    print(f"MAE: {mae}, RMSE: {rmse}, R²: {r2}")
+
+    # plot table of measures and average rmse compared to average price
+
+
+def main():
+    # collecting_data
+    mapped_vehicles_df = get_used_car_dataset()
+    decomp_vehicles_df = decomposing_used_car_data(mapped_vehicles_df)
+    info_train, info_test, price_train, price_test = split_test_and_train_data(decomp_vehicles_df)
+    
+    # training price prediction model
+    dt_price_model = creating_decision_tree(info_train, price_train)
+
+    # testing price prediction model
+    testing_decision_tree_model(dt_price_model, info_train, price_train)
+    testing_decision_tree_model(dt_price_model, info_test, price_test)
+
+main()

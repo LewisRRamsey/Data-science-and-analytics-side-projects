ce70183185bc038fc2544be24460b444db450a6e
LewisRRamsey <lewis.ramsey18@gmail.com>
2025-10-29 22:54:48 +0000

    cleaning up functions and editing comments

--- Data analysis side project on used cars/car_price_random_forrest_regressor_model.py
+++ Data analysis side project on used cars/car_price_random_forrest_regressor_model.py
@@ -25,14 +25,18 @@ def get_used_car_dataset():
     return mapped_vehicles_df
 
 
-# removing unnecessary data from used car dataframe, leaving price, mileage, condition, cylinders, manufacturer and year
 def decomposing_used_car_data(mapped_vehicles_df: pd.DataFrame):
+
+    # removing unnecessary data from used car dataframe, leaving price, mileage, condition, cylinders, manufacturer and year
+
     decomp_vehicles_df = mapped_vehicles_df[['price', 'year', 'manufacturer', 'condition', 'cylinders', 'odometer']]
     return decomp_vehicles_df
 
-# splitting data into test and training data using a 90/10 split
+
 def split_test_and_train_data(decomp_vehicles_df):
-    None
+
+    # splitting data into test and training data using a 90/10 split
+
     x = decomp_vehicles_df.drop(columns=['price']).values
     y = decomp_vehicles_df['price'].values
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
@@ -41,22 +45,30 @@ def split_test_and_train_data(decomp_vehicles_df):
 
 def creating_decision_tree(info_train, price_train):
 
-    # do further tweaks to model
+    # creating random forest regressor model
+    
     dt_price_model = RandomForestRegressor(random_state=42, max_depth = 23, max_features = 'sqrt', bootstrap = False, n_estimators = 100)
     dt_price_model.fit(info_train, price_train)
     return dt_price_model
 
 
 def testing_decision_tree_model(dt_price_model, info_test, info_train, price_test, price_train, mapped_vehicles_df):
+
+    # predicting prices with random forest regressor model
     price_pred_train = dt_price_model.predict(info_train)
     price_pred_test = dt_price_model.predict(info_test)
+
+    # calculating mae model
     train_mae = mean_absolute_error(price_train, price_pred_train)
     test_mae = mean_absolute_error(price_test, price_pred_test)
 
+    # calculating avg error %
     prices = mapped_vehicles_df['price']
     avg_price = np.mean(prices)
     avg_error_percentage_train = f'{(train_mae / avg_price) * 100}%'
     avg_error_percentage_test = f'{(test_mae / avg_price) * 100}%'
+
+    # plotting table
 
     fig, ax = plt.subplots()
     ax.axis('tight')
